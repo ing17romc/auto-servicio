3ffe1aae7e02bcb3fdf39747b2e4e55d9b0224aa
Rafael Márquez <ing17romc@gmail.com>
2023-02-15 00:26:39 -0600

    updated env

--- backend/mysql/conectionDB.js
+++ backend/mysql/conectionDB.js
@@ -1,13 +1,27 @@
+const DEV_MYSQL_HOST = process.env.DEV_MYSQL_HOST
+const DEV_MYSQL_PORT = process.env.DEV_MYSQL_PORT
+const DEV_MYSQL_DATABASE = process.env.DEV_MYSQL_DATABASE
+const DEV_MYSQL_USER = process.env.DEV_MYSQL_USER
+const DEV_MYSQL_PASSWORD = process.env.DEV_MYSQL_PASSWORD
+
+const PROD_MYSQL_HOST = process.env.PROD_MYSQL_HOST
+const PROD_MYSQL_PORT = process.env.PROD_MYSQL_PORT
+const PROD_MYSQL_DATABASE = process.env.PROD_MYSQL_DATABASE
+const PROD_MYSQL_USER = process.env.PROD_MYSQL_USER
+const PROD_MYSQL_PASSWORD = process.env.PROD_MYSQL_PASSWORD
+
 function connection () {
+	const DEV = process.env.NODE_ENV !== 'production'
+
 	try {
 		const mysql = require('mysql2')
 
 		const config = {
-			host: process.env.MYSQL_HOST,
-			port: process.env.MYSQL_PORT,
-			database: process.env.MYSQL_DATABASE,
-			user: process.env.MYSQL_USER,
-			password: process.env.MYSQL_PASSWORD
+			host: DEV ? DEV_MYSQL_HOST : PROD_MYSQL_HOST,
+			port: DEV ? DEV_MYSQL_PORT : PROD_MYSQL_PORT,
+			database: DEV ? DEV_MYSQL_DATABASE : PROD_MYSQL_DATABASE,
+			user: DEV ? DEV_MYSQL_USER : PROD_MYSQL_USER,
+			password: DEV ? DEV_MYSQL_PASSWORD : PROD_MYSQL_PASSWORD
 		}
 
 		const pool = mysql.createPool(config)
--- config/index.js
+++ config/index.js
@@ -1,5 +1,8 @@
+const DEV = process.env.NODE_ENV !== 'production'
+const DEV_URL = process.env.DEV_URL
+const PROD_URL = process.env.PROD_URL
 
-const BASE_URL = process.env.BASE_URL
+const BASE_URL = DEV ? DEV_URL : PROD_URL
 
 export const CONFIG = {
 	ROUTER: {
