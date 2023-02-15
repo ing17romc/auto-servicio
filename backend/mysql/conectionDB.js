9edc3b9aa6481887bc1f6725bef533e7a9b2d9a0
Rafael Márquez <ing17romc@gmail.com>
2023-02-14 23:47:54 -0600

    updated env

--- backend/mysql/conectionDB.js
+++ backend/mysql/conectionDB.js
@@ -1,28 +1,20 @@
 function connection () {
 	try {
 		const mysql = require('mysql2')
-		const DEV = process.env.NODE_ENV !== 'production'
-		if (DEV) {
-			const config = {
-				host: process.env.MYSQL.HOST,
-				port: process.env.MYSQL.PORT,
-				database: process.env.MYSQL.DATABASE,
-				user: process.env.MYSQL.USER,
-				password: process.env.MYSQL.PASSWORD
-			}
 
-			const pool = mysql.createPool(config)
-
-			const promisePool = pool.promise()
-
-			return promisePool
-		} else {
-			const pool = mysql.createPool(process.env.DATABASE_URL)
-
-			const promisePool = pool.promise()
-
-			return promisePool
+		const config = {
+			host: process.env.MYSQL_HOST,
+			port: process.env.MYSQL_PORT,
+			database: process.env.MYSQL_DATABASE,
+			user: process.env.MYSQL_USER,
+			password: process.env.MYSQL_PASSWORD
 		}
+
+		const pool = mysql.createPool(config)
+
+		const promisePool = pool.promise()
+
+		return promisePool
 	} catch (error) {
 		return console.log(`Could not connect - ${error}`)
 	}
--- config/index.js
+++ config/index.js
@@ -1,8 +1,5 @@
-const DEV = process.env.NODE_ENV !== 'production'
-const DEV_URL = process.env.DEV_URL
-const PROD_URL = process.env.PROD_URL
 
-const BASE_URL = DEV ? DEV_URL : PROD_URL
+const BASE_URL = process.env.BASE_URL
 
 export const CONFIG = {
 	ROUTER: {
