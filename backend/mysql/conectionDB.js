e51864ede28ebdb53cf9c970a7e8b186b76046ca
Rafael Márquez <ing17romc@gmail.com>
2023-02-15 10:17:09 -0600

    updated env conections db

--- backend/mysql/conectionDB.js
+++ backend/mysql/conectionDB.js
@@ -1,33 +1,25 @@
-const DEV_MYSQL_HOST = process.env.DEV_MYSQL_HOST
-const DEV_MYSQL_PORT = process.env.DEV_MYSQL_PORT
-const DEV_MYSQL_DATABASE = process.env.DEV_MYSQL_DATABASE
-const DEV_MYSQL_USER = process.env.DEV_MYSQL_USER
-const DEV_MYSQL_PASSWORD = process.env.DEV_MYSQL_PASSWORD
 
-const PROD_MYSQL_HOST = process.env.PROD_MYSQL_HOST
-const PROD_MYSQL_PORT = process.env.PROD_MYSQL_PORT
-const PROD_MYSQL_DATABASE = process.env.PROD_MYSQL_DATABASE
-const PROD_MYSQL_USER = process.env.PROD_MYSQL_USER
-const PROD_MYSQL_PASSWORD = process.env.PROD_MYSQL_PASSWORD
-
-function connection () {
+const getConfig = () => {
 	const DEV = process.env.NODE_ENV !== 'production'
 
+	if (DEV) {
+		return {
+			host: process.env.DEV_MYSQL_HOST,
+			port: process.env.DEV_MYSQL_PORT,
+			database: process.env.DEV_MYSQL_DATABASE,
+			user: process.env.DEV_MYSQL_USER,
+			password: process.env.DEV_MYSQL_PASSWORD
+		}
+	} else {
+		return process.env.DATABASE_URL
+	}
+}
+
+function connection () {
 	try {
 		const mysql = require('mysql2')
 
-		const config = {
-			host: DEV ? DEV_MYSQL_HOST : PROD_MYSQL_HOST,
-			port: DEV ? DEV_MYSQL_PORT : PROD_MYSQL_PORT,
-			database: DEV ? DEV_MYSQL_DATABASE : PROD_MYSQL_DATABASE,
-			user: DEV ? DEV_MYSQL_USER : PROD_MYSQL_USER,
-			password: DEV ? DEV_MYSQL_PASSWORD : PROD_MYSQL_PASSWORD,
-			dialectOptions: {
-				ssl: {
-					rejectUnauthorized: true
-				}
-			}
-		}
+		const config = getConfig()
 
 		const pool = mysql.createPool(config)
 
