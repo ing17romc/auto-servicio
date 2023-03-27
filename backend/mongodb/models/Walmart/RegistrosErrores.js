b43b6688b037b3cb32521bd1c167f5d059c3438f
Rafael Márquez <ing17romc@gmail.com>
2023-03-27 00:56:35 -0600

    updated log prod and dev

--- backend/mongodb/models/Walmart/RegistrosErrores.js
+++ backend/mongodb/models/Walmart/RegistrosErrores.js
@@ -1,16 +1,23 @@
 import mongoose from 'mongoose'
 import dbConnect from '../../'
+const DEV = process.env.NODE_ENV !== 'production'
+const collection = DEV ? 'AutoServicio_Log_DEV' : 'AutoServicio_Log_PROD'
 
 /* PetSchema will correspond to a collection in your MongoDB database. */
+console.log(collection)
+
 const LogSchema = new mongoose.Schema({
 	file: String,
 	sheet: String,
 	type: String,
 	message: String,
 	description: String
-})
+},
+{
+	timestamps: true
+}, { collection })
 
-const Log = mongoose.models.Log || mongoose.model('Log', LogSchema)
+const Log = mongoose.models.Log || mongoose.model('Log', LogSchema, collection)
 
 export const find = async () => {
 	await dbConnect()
